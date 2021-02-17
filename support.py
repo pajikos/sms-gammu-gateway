52f451e4c4493135dcd70558d770019168baa17c
Grégoire Pailler <gregoire@pailler.fr>
2021-02-17 10:46:36 +0800

    Add multi-part and encoded SMS support. Add new API endpoints to retrieve all SMS, retrieve/delete nth SMS, and retrieve network infos

--- support.py
+++ support.py
@@ -24,3 +24,51 @@ def init_state_machine(pin, filename='gammu.config'):
         else:
             sm.EnterSecurityCode('PIN', pin)
     return sm
+
+
+def retrieveAllSms(machine):
+    status = machine.GetSMSStatus()
+    allMultiPartSmsCount = status['SIMUsed'] + status['PhoneUsed'] + status['TemplatesUsed']
+
+    allMultiPartSms = []
+    start = True
+
+    while len(allMultiPartSms) < allMultiPartSmsCount:
+        if start:
+            currentMultiPartSms = machine.GetNextSMS(Start = True, Folder = 0)
+            start = False
+        else:
+            currentMultiPartSms = machine.GetNextSMS(Location = currentMultiPartSms[0]['Location'], Folder = 0)
+        allMultiPartSms.append(currentMultiPartSms)
+
+    allSms = gammu.LinkSMS(allMultiPartSms)
+
+    results = []
+    for sms in allSms:
+        smsPart = sms[0]
+
+        result = {
+            "Date": str(smsPart['DateTime']),
+            "Number": smsPart['Number'],
+            "State": smsPart['State'],
+            "Locations": [smsPart['Location'] for smsPart in sms],
+        }
+
+        decodedSms = gammu.DecodeSMS(sms)
+        if decodedSms == None:
+            result["Text"] = smsPart['Text']
+        else:
+            text = ""
+            for entry in decodedSms['Entries']:
+                if entry['Buffer'] != None:
+                    text += entry['Buffer']
+
+            result["Text"] = text
+
+        results.append(result)
+
+    return results
+
+
+def deleteSms(machine, sms):
+    list(map(lambda location: machine.DeleteSMS(Folder=0, Location=location), sms["Locations"]))
